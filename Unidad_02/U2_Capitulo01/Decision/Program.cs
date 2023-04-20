282796398433a2609261e38755dc39a652f8c138
Lucas Cordoba <lcordoba844@gmail.com>
2023-04-20 12:52:23 -0300

    Done with if/else if

--- Unidad_02/U2_Capitulo01/Decision/Program.cs
+++ Unidad_02/U2_Capitulo01/Decision/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+Console.WriteLine("Ingrese un texto.");
+String inputTexto = Console.ReadLine();
+
+if (inputTexto == null)
+{
+    Console.WriteLine("No se ha ingresado un texto válido");
+}
+else {
+    Console.WriteLine("Ingrese una opción:");
+    Console.WriteLine("1: Mostrar texto en mayúscula.");
+    Console.WriteLine("2: Mostrar texto en minúscula.");
+    Console.WriteLine("3: Mostrar cantidad de caracteres del texto ingresado.");
+    Console.WriteLine("");
+
+    ConsoleKeyInfo opcion = Console.ReadKey();
+
+    if (opcion.Key == ConsoleKey.D1)
+    {
+        Console.WriteLine("_ La palabra en mayúscula es: " + inputTexto.ToUpper());
+    }
+    else if (opcion.Key == ConsoleKey.D2)
+    {
+        Console.WriteLine("_ La palabra en minúscula es: " + inputTexto.ToLower());
+    }
+    else if (opcion.Key == ConsoleKey.D3)
+    {
+        Console.WriteLine("_ La longitud del texto es:" + inputTexto.Length);
+    }
+    else { 
+        Console.WriteLine("_ No seleccionó ninguna de las opciones.");    
+    }
+
+}
